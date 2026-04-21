f2efb0ca72f75fe6db2f008c6f2a7025683907b1
chikarimachi <podtsukuyomi@gmail.com>
2026-04-21 22:20:26 +0700

    Add gitignore and remove unnecessary files

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tasks(title, description, status)
-VALUES ('Подготовить проект', 'Создать стартовую структуру приложения', 'NEW');
-
-INSERT INTO tasks(title, description, status)
-VALUES ('Сделать документацию', 'Описать запуск и примеры запросов', 'IN_PROGRESS');
